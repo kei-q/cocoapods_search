e021078ef37c7672ceb150f90f17795e4054bcde
Yoshinori Kawasaki <ykawasaki@gmail.com>
2014-02-10 01:17:12 +0900

    Continue from 3505

--- lib/util/fetch_github_data.rb
+++ lib/util/fetch_github_data.rb
@@ -1,6 +1,6 @@
-PodLibrary.where("id > 1728").find_each(batch_size: 100) do |pod|
+PodLibrary.where("id > 3505").find_each(batch_size: 100) do |pod|
   puts "Checking #{pod.name}"
   success = pod.fetch_github_repo_data(update_repo: false, update_commits: true, update_contributors: true, update_releases: true)
   puts "Failed #{pod.name}" unless success
-  sleep 0.3
+  sleep 0.4
 end
