6aa05beb3548f4fc5628d9c5f0cba2c27360161b
Yoshinori Kawasaki <ykawasaki@gmail.com>
2014-02-09 17:11:40 +0900

    Add root path

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
+  root 'pod_libraries#index'
+
   resources :pod_libraries
+
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
