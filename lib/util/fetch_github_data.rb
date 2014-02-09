1e501a7e1aeb671fbad08d170358b9481b7cd5f6
Yoshinori Kawasaki <ykawasaki@gmail.com>
2014-02-10 07:31:31 +0900

    Continue

--- lib/util/fetch_github_data.rb
+++ lib/util/fetch_github_data.rb
@@ -1,6 +1,7 @@
-PodLibrary.find_each(batch_size: 20) do |pod|
+cont = PodLibrary.where(name: 'SCKit').first.id
+PodLibrary.where("id > ?", cont).find_each(batch_size: 20) do |pod|
   puts "Checking #{pod.name}"
   success = pod.fetch_github_repo_data(update_repo: false, update_commits: false, update_contributors: false, update_releases: true)
   puts "Failed #{pod.name}" unless success
-  sleep 0.5
+  # sleep 0.5
 end
