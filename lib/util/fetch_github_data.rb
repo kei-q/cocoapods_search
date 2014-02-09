eb8efd1058e41a888e5438455b5212debd63db32
Yoshinori Kawasaki <ykawasaki@gmail.com>
2014-02-10 00:41:32 +0900

    Continue from 1729...

--- lib/util/fetch_github_data.rb
+++ lib/util/fetch_github_data.rb
@@ -1,4 +1,4 @@
-PodLibrary.find_each(batch_size: 100) do |pod|
+PodLibrary.where("id > 1728").find_each(batch_size: 100) do |pod|
   puts "Checking #{pod.name}"
   success = pod.fetch_github_repo_data(update_repo: false, update_commits: true, update_contributors: true, update_releases: true)
   puts "Failed #{pod.name}" unless success
