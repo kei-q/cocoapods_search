a816c7489b2c6d715eb88b1dd2ef4f06e3114f1c
Yoshinori Kawasaki <ykawasaki@gmail.com>
2014-02-10 00:40:23 +0900

    Sleep

--- lib/util/fetch_github_data.rb
+++ lib/util/fetch_github_data.rb
@@ -2,4 +2,5 @@ PodLibrary.find_each(batch_size: 100) do |pod|
   puts "Checking #{pod.name}"
   success = pod.fetch_github_repo_data(update_repo: false, update_commits: true, update_contributors: true, update_releases: true)
   puts "Failed #{pod.name}" unless success
+  sleep 0.3
 end
