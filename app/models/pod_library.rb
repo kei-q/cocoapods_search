fefddf9a0f3ba27e6a104d10d386dff78c711d7b
Yoshinori Kawasaki <ykawasaki@gmail.com>
2014-02-09 16:54:12 +0900

    Add methods to load CocoaPods/Specs repo

--- app/models/pod_library.rb
+++ app/models/pod_library.rb
@@ -1,2 +1,32 @@
 class PodLibrary < ActiveRecord::Base
+  def self.sets
+    @sets ||= Pod::Source.new(path).pod_sets
+  end
+
+  def self.prepare(force: false)
+    if force || empty?
+      get
+      unpack
+    end
+  end
+
+  private
+  def self.path
+    './tmp/specs'
+  end
+
+  def self.empty?
+    Dir['./tmp/specs/*'].empty?
+  end
+
+  def self.get
+    `curl -L -o ./tmp/specs.tar.gz http://github.com/CocoaPods/Specs/tarball/master`
+  end
+
+  def self.unpack
+    `rm -rf ./tmp/specs`
+    `gunzip -f ./tmp/specs.tar.gz`
+    `cd tmp; tar xvf specs.tar`
+    `mv -f ./tmp/CocoaPods-Specs-* ./tmp/specs`
+  end
 end
