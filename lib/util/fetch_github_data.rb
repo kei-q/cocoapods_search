90741111bcff292e614ad8b05f231070f2b12d2b
Yoshinori Kawasaki <ykawasaki@gmail.com>
2014-02-10 03:42:26 +0900

    Refresh release date

--- lib/util/fetch_github_data.rb
+++ lib/util/fetch_github_data.rb
@@ -1,6 +1,6 @@
 PodLibrary.find_each(batch_size: 20) do |pod|
   puts "Checking #{pod.name}"
-  success = pod.fetch_github_repo_data(update_repo: false, update_commits: false, update_contributors: false, update_releases: false)
+  success = pod.fetch_github_repo_data(update_repo: false, update_commits: false, update_contributors: false, update_releases: true)
   puts "Failed #{pod.name}" unless success
-  # sleep 0.4
+  sleep 0.5
 end
