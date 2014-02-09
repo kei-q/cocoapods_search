434cf03ae2471c3b7998f9969c0fae1604b52439
Yoshinori Kawasaki <ykawasaki@gmail.com>
2014-02-10 02:52:21 +0900

    Update fetch script

--- lib/util/fetch_github_data.rb
+++ lib/util/fetch_github_data.rb
@@ -1,6 +1,6 @@
-PodLibrary.where("id > 3505").find_each(batch_size: 100) do |pod|
+PodLibrary.find_each(batch_size: 20) do |pod|
   puts "Checking #{pod.name}"
-  success = pod.fetch_github_repo_data(update_repo: false, update_commits: true, update_contributors: true, update_releases: true)
+  success = pod.fetch_github_repo_data(update_repo: false, update_commits: false, update_contributors: false, update_releases: false)
   puts "Failed #{pod.name}" unless success
-  sleep 0.4
+  # sleep 0.4
 end
