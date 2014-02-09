62d6f58ecd49183928f417a011b8df39b226c167
Yoshinori Kawasaki <ykawasaki@gmail.com>
2014-02-09 16:56:03 +0900

    Add github related methods to Pod::Specification class

---
+++ config/initializers/pod.rb
@@ -0,0 +1,24 @@
+class Pod::Specification
+  class << self
+    def github_client
+      @client ||= Octokit::Client.new(oauth_token: ENV['GITHUB_TOKEN'])
+    end
+  end
+
+  def git_repo
+    source[:git]
+  end
+
+  def github?
+    git_repo && git_repo.start_with?('https://github.com')
+  end
+
+  def github_repo_name
+    match_data = git_repo.match("https://github.com/(.+?)/(.+?).git")
+    "#{match_data[1]}/#{match_data[2]}"
+  end
+
+  def github_repo
+    @repo ||= self.class.github_client.repo(github_repo_name)
+  end
+end
