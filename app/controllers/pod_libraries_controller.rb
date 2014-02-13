f617589d8af0d8858f36fd03939f5d7d4b481d4d
Yoshinori Kawasaki <ykawasaki@gmail.com>
2014-02-14 00:24:55 +0900

    Tidy up routes and controller

--- app/controllers/pod_libraries_controller.rb
+++ app/controllers/pod_libraries_controller.rb
@@ -9,60 +9,6 @@ class PodLibrariesController < ApplicationController
     @pods = pods_scope.page(params[:page])
   end
 
-  # GET /pod_libraries/1
-  # GET /pod_libraries/1.json
-  def show
-  end
-
-  # GET /pod_libraries/new
-  def new
-    @pod_library = PodLibrary.new
-  end
-
-  # GET /pod_libraries/1/edit
-  def edit
-  end
-
-  # POST /pod_libraries
-  # POST /pod_libraries.json
-  def create
-    @pod_library = PodLibrary.new(pod_library_params)
-
-    respond_to do |format|
-      if @pod_library.save
-        format.html { redirect_to @pod_library, notice: 'Pod library was successfully created.' }
-        format.json { render action: 'show', status: :created, location: @pod_library }
-      else
-        format.html { render action: 'new' }
-        format.json { render json: @pod_library.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # PATCH/PUT /pod_libraries/1
-  # PATCH/PUT /pod_libraries/1.json
-  def update
-    respond_to do |format|
-      if @pod_library.update(pod_library_params)
-        format.html { redirect_to @pod_library, notice: 'Pod library was successfully updated.' }
-        format.json { head :no_content }
-      else
-        format.html { render action: 'edit' }
-        format.json { render json: @pod_library.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # DELETE /pod_libraries/1
-  # DELETE /pod_libraries/1.json
-  def destroy
-    @pod_library.destroy
-    respond_to do |format|
-      format.html { redirect_to pod_libraries_url }
-      format.json { head :no_content }
-    end
-  end
-
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_pod_library
