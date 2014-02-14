517eb0c1d2e47e2b98589ff9bde4d4b7149bd280
Yoshinori Kawasaki <ykawasaki@gmail.com>
2014-02-14 12:17:59 +0900

    Execute data clean up in prod

--- lib/util/fetch_github_data.rb
+++ lib/util/fetch_github_data.rb
@@ -1,7 +1,7 @@
-cont = PodLibrary.where(name: 'SCKit').first.id
+cont = 0  # PodLibrary.where(name: 'SCKit').first.id
 PodLibrary.where("id > ?", cont).find_each(batch_size: 20) do |pod|
   puts "Checking #{pod.name}"
-  success = pod.fetch_github_repo_data(update_repo: false, update_commits: false, update_contributors: false, update_releases: true)
+  success = pod.fetch_github_repo_data(update_repo: false, update_commits: false, update_contributors: false, update_releases: false)
   puts "Failed #{pod.name}" unless success
   # sleep 0.5
 end
