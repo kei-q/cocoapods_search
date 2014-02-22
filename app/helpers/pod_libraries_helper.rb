922310235d924cd178a637af1598a770308095e0
Yoshinori Kawasaki <ykawasaki@gmail.com>
2014-02-23 00:23:02 +0900

    Add icons to order options

--- app/helpers/pod_libraries_helper.rb
+++ app/helpers/pod_libraries_helper.rb
@@ -1,2 +1,11 @@
 module PodLibrariesHelper
+  ICON_CLASSES = {
+    popularity:   'fi-heart',
+    contributors: 'fi-social-github',
+    stars:        'fi-star',
+    last_commit:  'fi-clock',
+  }.freeze
+  def icon_class(order_type)
+    ICON_CLASSES[order_type.to_sym]
+  end
 end
