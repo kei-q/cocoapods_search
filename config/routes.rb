616faa4720585ee9b12815cd4d9b6bb6d7144a22
Yoshinori Kawasaki <ykawasaki@gmail.com>
2014-04-29 17:18:34 +0900

    Limit to authors#show for now

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   root 'pod_libraries#index'
 
   resources :pods, controller: :pod_libraries, only: [:index, :show]
-  resources :authors
+  resources :authors, only: [:show]
 end
