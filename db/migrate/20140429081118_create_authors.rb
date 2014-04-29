6f252c3760491d3fb40dd82f9868834c768cacd6
Yoshinori Kawasaki <ykawasaki@gmail.com>
2014-04-29 17:17:17 +0900

    Add columns to authors and authorings

--- db/migrate/20140429081118_create_authors.rb
+++ db/migrate/20140429081118_create_authors.rb
@@ -1,7 +1,8 @@
 class CreateAuthors < ActiveRecord::Migration
   def change
     create_table :authors do |t|
-
+      t.string :name, null: false
+      t.string :email
       t.timestamps
     end
   end
